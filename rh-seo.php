590241264390ac213df2525b6dec670a6abfb79b
Robin Herbeck <info@robinherbeck.com>
2026-06-17 12:47:22 +0200

    Core 2.4.1 bundeln: Loader-Fix fuer gemischte Versionen

--- rh-seo.php
+++ rh-seo.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/herbeckrobin/rh-seo
  * Update URI:        https://github.com/herbeckrobin/rh-seo
  * Description:       SEO-Grundgerüst: Firmen-Stammdaten als Single Source, JSON-LD-Graph, Meta-/Open-Graph-Tags, Canonical, Sitemap-/Robots-/Lang-Cleanup. Teil der rh-blueprint Kollektion.
- * Version:           0.2.0
+ * Version:           0.1.2
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Robin Herbeck
@@ -21,7 +21,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('RHSEO_VERSION', '0.2.0');
+define('RHSEO_VERSION', '0.1.2');
 define('RHSEO_PLUGIN_FILE', __FILE__);
 define('RHSEO_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
